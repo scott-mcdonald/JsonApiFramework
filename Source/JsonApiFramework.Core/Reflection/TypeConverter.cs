1749cd819ec1396a8940b47c145ece8fbd2cb847
Scott McDonald <scott@quantumsoft.com>
2016-08-25 20:28:11 -0400

    Fix #11 Change TypeConverter so null converts to default(T)
    Signed-off-by: Scott McDonald <scott@quantumsoft.com>

--- Source/JsonApiFramework.Core/Reflection/TypeConverter.cs
+++ Source/JsonApiFramework.Core/Reflection/TypeConverter.cs
@@ -16,19 +16,38 @@ namespace JsonApiFramework.Reflection
         {
             Contract.Requires(targetType != null);
 
+            object targetValue;
+            ConvertResult result;
             try
             {
-                object targetValue;
-                if (TryConvertInternal(sourceValue, targetType, out targetValue, targetFormatProvider))
-                {
-                    return targetValue;
-                }
-
-                throw new TypeConverterException(BuildTypeConverterMessage(sourceValue, targetType));
+                result = TryConvertInternal(sourceValue, targetType, targetFormatProvider, out targetValue);
             }
             catch (Exception exception)
             {
                 throw new TypeConverterException(BuildTypeConverterMessage(sourceValue, targetType), exception);
+            }
+
+            switch (result)
+            {
+                case ConvertResult.Success:
+                    {
+                        return targetValue;
+                    }
+
+                case ConvertResult.SuccessReturnDefault:
+                    {
+                        // Has to be a value type, therefore create default
+                        // value type with the always available default constructor.
+                        return Activator.CreateInstance(targetType);
+                    }
+
+                case ConvertResult.FailedThrowException:
+                    {
+                        throw new TypeConverterException(BuildTypeConverterMessage(sourceValue, targetType));
+                    }
+
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
 
@@ -38,7 +57,23 @@ namespace JsonApiFramework.Reflection
 
             try
             {
-                return TryConvertInternal(sourceValue, targetType, out targetValue, targetFormatProvider);
+                var result = TryConvertInternal(sourceValue, targetType, targetFormatProvider, out targetValue);
+                switch (result)
+                {
+                    case ConvertResult.Success:
+                    case ConvertResult.SuccessReturnDefault:
+                        {
+                            return true;
+                        }
+
+                    case ConvertResult.FailedThrowException:
+                        {
+                            return false;
+                        }
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
             }
             catch (Exception)
             {
@@ -52,21 +87,76 @@ namespace JsonApiFramework.Reflection
         public static T Convert<T>(object sourceValue, IFormatProvider targetFormatProvider = null)
         {
             var targetType = typeof(T);
-            return (T)Convert(sourceValue, targetType, targetFormatProvider);
+            object targetValue;
+            ConvertResult result;
+            try
+            {
+                result = TryConvertInternal(sourceValue, targetType, targetFormatProvider, out targetValue);
+            }
+            catch (Exception exception)
+            {
+                throw new TypeConverterException(BuildTypeConverterMessage(sourceValue, targetType), exception);
+            }
+
+            switch (result)
+            {
+                case ConvertResult.Success:
+                    {
+                        return (T)targetValue;
+                    }
+
+                case ConvertResult.SuccessReturnDefault:
+                    {
+                        // Has to be a value type, therefore return default(T).
+                        return default(T);
+                    }
+
+                case ConvertResult.FailedThrowException:
+                    {
+                        throw new TypeConverterException(BuildTypeConverterMessage(sourceValue, targetType));
+                    }
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
 
         public static bool TryConvert<T>(object sourceValue, out T targetValue, IFormatProvider targetFormatProvider = null)
         {
             var targetType = typeof(T);
-            object targetValueAsObject;
-            if (TryConvert(sourceValue, targetType, out targetValueAsObject, targetFormatProvider))
+            try
             {
-                targetValue = (T)targetValueAsObject;
-                return true;
-            }
+                object targetValueAsObject;
+                var result = TryConvertInternal(sourceValue, targetType, targetFormatProvider, out targetValueAsObject);
+                switch (result)
+                {
+                    case ConvertResult.Success:
+                        {
+                            targetValue = (T)targetValueAsObject;
+                            return true;
+                        }
 
-            targetValue = default(T);
-            return false;
+                    case ConvertResult.SuccessReturnDefault:
+                        {
+                            targetValue = default(T);
+                            return true;
+                        }
+
+                    case ConvertResult.FailedThrowException:
+                        {
+                            targetValue = default(T);
+                            return false;
+                        }
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            catch (Exception)
+            {
+                targetValue = default(T);
+                return false;
+            }
         }
         #endregion
 
@@ -82,125 +172,6 @@ namespace JsonApiFramework.Reflection
             var targetTypeAsString = targetType.Name;
 
             return "Unable to convert from {0} to {1}".FormatWith(sourceTypeAsString, targetTypeAsString);
-        }
-
-        private static bool IsSystemConvertChangeTypeCapableOrIsEnum(Type type)
-        {
-            Contract.Requires(type != null);
-
-            var isAvailableSystemConvertChangeTypes = AvailableSystemConvertChangeTypes.Contains(type);
-            var isEnum = type.IsEnum();
-
-            return isAvailableSystemConvertChangeTypes || isEnum;
-        }
-
-        private static bool TryConvertInternal(object sourceValue, Type targetType, out object targetValue, IFormatProvider targetFormatProvider)
-        {
-            Contract.Requires(targetType != null);
-
-            targetValue = null;
-            targetFormatProvider = targetFormatProvider ?? CultureInfo.InvariantCulture;
-
-            // Handle case when source object is null.
-            if (sourceValue == null)
-            {
-                // Handle case when target is a Nullable<T> type.
-                if (targetType.IsNullableType())
-                {
-                    // Target is a Nullable<T> type which allows conversion to null.
-                    return true;
-                }
-
-                // Handle case when target is a value type.
-                if (targetType.IsValueType())
-                {
-                    // Target type can not be a value type as value types can not be null.
-                    throw new InvalidOperationException("Unable to convert value type [name={0}] to a null object.".FormatWith(targetType.Name));
-                }
-
-                // Target must be a reference type which allows conversion to null.
-                return true;
-            }
-
-            // Handle case when target and source are the exact same type.
-            var sourceType = sourceValue.GetType();
-            if (sourceType == targetType)
-            {
-                targetValue = sourceValue;
-                return true;
-            }
-
-            // Handle case when target is a Nullable<T> type. Special CLR
-            // rules for Nullable<T> and boxing:
-            // 1. If source is null, then no boxing occurs and null is returned.
-            // 2. If source is not null, then a boxed value type is returned; not a boxed Nullable<T> type.
-            // Therefore the source is null case is handled previously, the
-            if (targetType.IsNullableType())
-            {
-                targetType = Nullable.GetUnderlyingType(targetType);
-
-                // Handle case when source and the underlying nullable target
-                // are the exact same type.
-                if (sourceType == targetType)
-                {
-                    targetValue = sourceValue;
-                    return true;
-                }
-            }
-
-            // Handle case when target is assignable from source.
-            if (targetType.IsAssignableFrom(sourceType))
-            {
-                targetValue = sourceValue;
-                return true;
-            }
-
-            // Handle case when source and target types are convertible with
-            // Convert.ChangeType .NET system function or is an Enum.
-            if (IsSystemConvertChangeTypeCapableOrIsEnum(sourceType) && IsSystemConvertChangeTypeCapableOrIsEnum(targetType))
-            {
-                // Handle special case when source type is a string or integer and converting to an enumeration.
-                if (targetType.IsEnum())
-                {
-                    if (sourceType.IsStringType())
-                    {
-                        targetValue = Enum.Parse(targetType, (string)sourceValue, true);
-                        return true;
-                    }
-
-                    if (sourceType.IsIntegralType())
-                    {
-                        targetValue = Enum.ToObject(targetType, sourceValue);
-                        return true;
-                    }
-                }
-
-                targetValue = System.Convert.ChangeType(sourceValue, targetType, targetFormatProvider);
-                return true;
-            }
-
-            // Handle case string => Guid, Uri, DateTimeOffset, TimeSpan, byte[], Type
-            if (HandleStringToSpecialCaseTypes(sourceValue, targetType, targetFormatProvider, ref targetValue))
-                return true;
-
-            // Handle case Guid, Uri, DateTimeOffset, TimeSpan, byte[], Type => string
-            if (HandleSpecialCaseTypesToString(sourceValue, targetType, ref targetValue))
-                return true;
-
-            // Handle case DateTime => DateTimeOffset or DateTimeOffset => DateTime
-            if (HandleSpecialCaseTypesOfDateTimeAndDateTimeOffset(sourceValue, targetType, ref targetValue))
-                return true;
-
-            // Handle case byte[] => Guid or Guid => byte[]
-            if (HandleSpecialCaseTypesOfByteArrayAndGuid(sourceValue, targetType, ref targetValue))
-                return true;
-
-            // Handle case some derived Type => Type
-            if (HandleSpecialCaseTypesOfTypeAndDerivedType(sourceValue, targetType, ref targetValue))
-                return true;
-
-            // Can not convert from source to target types.
-            return false;
         }
 
         private static bool HandleStringToSpecialCaseTypes(object sourceValue, Type targetType, IFormatProvider targetFormatProvider, ref object targetValue)
@@ -350,9 +321,147 @@ namespace JsonApiFramework.Reflection
 
             return false;
         }
+
+        private static bool IsSystemConvertChangeTypeCapableOrIsEnum(Type type)
+        {
+            Contract.Requires(type != null);
+
+            var isAvailableSystemConvertChangeTypes = AvailableSystemConvertChangeTypes.Contains(type);
+            var isEnum = type.IsEnum();
+
+            return isAvailableSystemConvertChangeTypes || isEnum;
+        }
+
+        private static void ThrowTypeConverterException(object sourceValue, Type targetType, Exception exception)
+        {
+            if (exception == null)
+            {
+                throw new TypeConverterException(BuildTypeConverterMessage(sourceValue, targetType));    
+            }
+
+            throw new TypeConverterException(BuildTypeConverterMessage(sourceValue, targetType), exception);
+        }
+
+        private static ConvertResult TryConvertInternal(object sourceValue, Type targetType, IFormatProvider targetFormatProvider, out object targetValue)
+        {
+            Contract.Requires(targetType != null);
+
+            targetValue = null;
+            targetFormatProvider = targetFormatProvider ?? CultureInfo.InvariantCulture;
+
+            // Handle case when source object is null.
+            if (sourceValue == null)
+            {
+                // Handle case when target is a Nullable<T> type.
+                if (targetType.IsNullableType())
+                {
+                    // Target is a Nullable<T> type which allows conversion to null.
+                    return ConvertResult.Success;
+                }
+
+                // Handle case when target is a value type.
+                if (targetType.IsValueType())
+                {
+                    // Target type can not be a value type as value types can not be null.
+                    return ConvertResult.SuccessReturnDefault;
+                }
+
+                // Target must be a reference type which allows conversion to null.
+                return ConvertResult.Success;
+            }
+
+            // Handle case when target and source are the exact same type.
+            var sourceType = sourceValue.GetType();
+            if (sourceType == targetType)
+            {
+                targetValue = sourceValue;
+                return ConvertResult.Success;
+            }
+
+            // Handle case when target is a Nullable<T> type. Special CLR
+            // rules for Nullable<T> and boxing:
+            // 1. If source is null, then no boxing occurs and null is returned.
+            // 2. If source is not null, then a boxed value type is returned; not a boxed Nullable<T> type.
+            // Therefore the source is null case is handled previously, the
+            if (targetType.IsNullableType())
+            {
+                targetType = Nullable.GetUnderlyingType(targetType);
+
+                // Handle case when source and the underlying nullable target
+                // are the exact same type.
+                if (sourceType == targetType)
+                {
+                    targetValue = sourceValue;
+                    return ConvertResult.Success;
+                }
+            }
+
+            // Handle case when target is assignable from source.
+            if (targetType.IsAssignableFrom(sourceType))
+            {
+                targetValue = sourceValue;
+                return ConvertResult.Success;
+            }
+
+            // Handle case when source and target types are convertible with
+            // Convert.ChangeType .NET system function or is an Enum.
+            if (IsSystemConvertChangeTypeCapableOrIsEnum(sourceType) && IsSystemConvertChangeTypeCapableOrIsEnum(targetType))
+            {
+                // Handle special case when source type is a string or integer and converting to an enumeration.
+                if (targetType.IsEnum())
+                {
+                    if (sourceType.IsStringType())
+                    {
+                        targetValue = Enum.Parse(targetType, (string)sourceValue, true);
+                        return ConvertResult.Success;
+                    }
+
+                    if (sourceType.IsIntegralType())
+                    {
+                        targetValue = Enum.ToObject(targetType, sourceValue);
+                        return ConvertResult.Success;
+                    }
+                }
+
+                targetValue = System.Convert.ChangeType(sourceValue, targetType, targetFormatProvider);
+                return ConvertResult.Success;
+            }
+
+            // Handle case string => Guid, Uri, DateTimeOffset, TimeSpan, byte[], Type
+            if (HandleStringToSpecialCaseTypes(sourceValue, targetType, targetFormatProvider, ref targetValue))
+                return ConvertResult.Success;
+
+            // Handle case Guid, Uri, DateTimeOffset, TimeSpan, byte[], Type => string
+            if (HandleSpecialCaseTypesToString(sourceValue, targetType, ref targetValue))
+                return ConvertResult.Success;
+
+            // Handle case DateTime => DateTimeOffset or DateTimeOffset => DateTime
+            if (HandleSpecialCaseTypesOfDateTimeAndDateTimeOffset(sourceValue, targetType, ref targetValue))
+                return ConvertResult.Success;
+
+            // Handle case byte[] => Guid or Guid => byte[]
+            if (HandleSpecialCaseTypesOfByteArrayAndGuid(sourceValue, targetType, ref targetValue))
+                return ConvertResult.Success;
+
+            // Handle case some derived Type => Type
+            if (HandleSpecialCaseTypesOfTypeAndDerivedType(sourceValue, targetType, ref targetValue))
+                return ConvertResult.Success;
+
+            // Can not convert from source to target types.
+            return ConvertResult.FailedThrowException;
+        }
         #endregion
 
         // PRIVATE FIELDS ///////////////////////////////////////////////////
+        #region Enumerations
+        private enum ConvertResult
+        {
+            FailedThrowException,
+            Success,
+            SuccessReturnDefault, // used for value types
+        }
+        #endregion
+
         #region Fields
         private static readonly ISet<Type> AvailableSystemConvertChangeTypes = new HashSet<Type>
             {
